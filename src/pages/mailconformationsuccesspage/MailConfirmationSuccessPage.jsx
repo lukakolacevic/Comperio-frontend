c6455a98c6dba2e82d6fed41cd34d5e7a64e93fc
lukakolacevic <lukakolacevic@gmail.com>
2024-12-23 00:11:42 +0100

    edited mail confirrm success page

--- src/pages/mailconformationsuccesspage/MailConfirmationSuccessPage.jsx
+++ src/pages/mailconformationsuccesspage/MailConfirmationSuccessPage.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button } from 'primereact/button';
 import './MailConfirmationSuccessPage.css';
 
 const MailConfirmationSuccessPage = () => {
   const navigate = useNavigate();
 
   return (
-
-    <div className="confirmation-success-wrapper">
-      <div className="confirmation-success-container">
-        
-        <h1>
-          Email potvrđen!
+    <div className="confirmationSuccessWrapper">
+      <div className="confirmationSuccessContainer">
+        <h1 className="successHeader">
+          ✅ Email potvrđen!
         </h1>
-        <p>Mail ti je potvrđen. Sad se možeš ulogirati i početi koristiti platformu.</p>
-        <Button label="Prijavi se" className="p-button-rounded p-button-outlined resend-button" onClick={() => navigate("/login")}/>
+        <p>
+          Mail ti je potvrđen. Sad se možeš ulogirati i početi koristiti platformu.
+        </p>
+        <button
+          className="primaryButton"
+          onClick={() => navigate("/login")}
+        >
+          Prijavi se
+        </button>
       </div>
     </div>
-
   );
 };
 
