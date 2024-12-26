a1230ec8a4f1b664d8a591b4e34d3b776049d43c
lukakolacevic <lukakolacevic@gmail.com>
2024-12-26 16:15:40 +0100

    added calendar to student homepage

--- src/pages/homepage/StudentHomePage.jsx
+++ src/pages/homepage/StudentHomePage.jsx
@@ -1,24 +1,24 @@
 import "./HomePage.css";
-
 import { Link } from "react-router-dom";
-import { useState, useEffect } from 'react';
-
-import InputAdornment from "@mui/material/InputAdornment";
+import { useState, useEffect } from "react";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
 import InstructorsComponent from "../../components/instructors/InstructorsComponent";
-import { getSubjects } from '../../api/SubjectApi';
-import { getInstructors } from '../../api/InstructorApi';
+import { getSubjects } from "../../api/SubjectApi";
+import { getInstructors, getStudentSessions } from "../../api/InstructorApi";
+import { Calendar } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { momentLocalizer } from "react-big-calendar";
+import moment from "moment";
 
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
-
+// Calendar localizer
+const localizer = momentLocalizer(moment);
 
 function ComboBox() {
   const [subjects, setSubjects] = useState([]);
 
-  console.log(localStorage.getItem("user"))
-
   useEffect(() => {
-    getSubjects().then(response => setSubjects(response.subjects));
+    getSubjects().then((response) => setSubjects(response.subjects));
   }, []);
 
   const handleSubjectSelect = (event, value) => {
@@ -28,93 +28,101 @@ function ComboBox() {
   };
 
   return (
-    <>
     <div className="search-container">
-    <Autocomplete
-      disablePortal
-      id="combo-box-demo"
-      options={subjects}
-      getOptionLabel={(option) => option.title}
-      onChange={handleSubjectSelect}
-      renderInput={(params) =>
-        <TextField {...params} 
-        InputProps={{
-          ...params.InputProps,
-          startAdornment: (
-            <InputAdornment position="start">
-              <img
-                src="/icons/search-icon.svg"
-                style={{ height: "20px", width: "20px" }}
-              />
-            </InputAdornment>
-          ),
-        }}
-        />
-      }
-    />
-    {/* <Button variant="contained" onClick={handleButtonClick}>Pretraži</Button> */}
+      <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        options={subjects}
+        getOptionLabel={(option) => option.title}
+        onChange={handleSubjectSelect}
+        renderInput={(params) => (
+          <TextField {...params} label="Search Subjects" variant="outlined" />
+        )}
+      />
     </div>
-    <div>
-      {subjects.map((subject) => (
-        <Link
-          to={`/subject/${subject.url}`}
-          key={subject.url}
-          className="link-no-style"
-        >
-          <div className="predmet">
-            <h2 className="predmet-text">{subject.title}</h2>
-            <p className="predmet-text">{subject.description}</p>
-          </div>
-        </Link>
-      ))}
-    </div>
-    </>
   );
 }
 
 function StudentHomePage() {
   const [instructors, setInstructors] = useState([]);
+  const [sessions, setSessions] = useState([]);
+  const user = localStorage.getItem("user");
 
   useEffect(() => {
     const fetchInstructors = async () => {
       const fetchedInstructors = await getInstructors();
-      console.log("Prvi return: ", fetchedInstructors); // Check what is actually being returned here
       setInstructors(fetchedInstructors.instructors);
-      console.log("Drugi return: ", instructors); // This will show the previous state due to closure
+    };
+
+    const fetchSessions = async () => {
+      try {
+        const response = await getStudentSessions(JSON.parse(user).id);
+        setSessions([
+          ...response.pastSessions.map((session) => ({
+            id: session.sessionId,
+            title: `${session.subject.title} (${session.user.name} ${session.user.surname})`,
+            start: new Date(session.dateTime),
+            end: new Date(session.dateTimeEnd),
+          })),
+          ...response.upcomingSessions.map((session) => ({
+            id: session.sessionId,
+            title: `${session.subject.title} (${session.user.name} ${session.user.surname})`,
+            start: new Date(session.dateTime),
+            end: new Date(session.dateTimeEnd),
+          })),
+        ]);
+      } catch (error) {
+        console.error("Failed to fetch sessions:", error);
+      }
     };
 
     fetchInstructors();
+    fetchSessions();
   }, []);
 
-  
-
   return (
-    <>
-      <div className="homepage-wrapper">
-        <div className="homepage-container">
-          <div>
-            <div className="title">
-              
-              <h2>Instrukcije po mjeri!</h2>
-            </div>
-
-              <ComboBox />
-
-          </div>
-
-          <div>
-            <h4>Najpopularniji instruktori:</h4>
-            <InstructorsComponent
-              instructors={instructors}
-              sessions = {null}
-              showSubject={true}
-              showInstructionsCount={true}
-              isOnProfilePage={false}
+    <div className="homepage-wrapper">
+      <div className="homepage-container">
+        {/* Left Column */}
+        <div className="left-column">
+          <div className="calendar-container">
+            <h3 className="title">Tvoji budući i prošli termini</h3>
+            <Calendar
+              localizer={localizer}
+              events={sessions}
+              startAccessor="start"
+              endAccessor="end"
+              style={{ height: 500 }}
+              onSelectEvent={(event) =>
+                alert(`You clicked on session: ${event.title}`)
+              }
+              eventPropGetter={() => ({
+                style: {
+                  backgroundColor: "#00a3f0",
+                  color: "white",
+                  borderRadius: "8px",
+                  border: "none",
+                },
+              })}
             />
           </div>
+          <h3 className="title">Pretraži predmete</h3>
+          <ComboBox />
+        </div>
+
+        {/* Right Column */}
+        <div className="right-column">
+          <h4 className="title">Najpopularniji instruktori:</h4>
+          <InstructorsComponent
+            instructors={instructors}
+            sessions={null}
+            showSubject={true}
+            showInstructionsCount={true}
+            isOnProfilePage={false}
+          />
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
