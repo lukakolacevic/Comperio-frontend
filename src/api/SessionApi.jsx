343868fed7f31a69d026d5b26a7cf593f33a7db7
lukakolacevic <lukakolacevic@gmail.com>
2025-01-07 17:41:43 +0100

    added frontend call to edit session note

--- src/api/SessionApi.jsx
+++ src/api/SessionApi.jsx
@@ -1,3 +1,5 @@
+import { makeRequestWithRetry } from "./MakeRequest";
+
 export async function getSessionDetails(sessionId) {
   const url = `${import.meta.env.VITE_REACT_BACKEND_URL}/sessions/${sessionId}`;
   const options = {
@@ -8,3 +10,13 @@ export async function getSessionDetails(sessionId) {
   return makeRequestWithRetry(url, options);
 }
 
+export async function editSessionNote(sessionId, newNote){
+  const url = `${import.meta.env.VITE_REACT_BACKEND_URL}/sessions/${sessionId}/edit-note`;
+  const options = {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+    body: JSON.stringify({ newNote: newNote })
+  };
+  return makeRequestWithRetry(url, options);
+}
